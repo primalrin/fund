3f86911a6a87f663f2d700961e32ccf3ed542e14
primalrin <shestkon@gmail.com>
2024-10-19 01:08:40 +0300

    err with c, d summ fix

--- lab1/ex5.c
+++ lab1/ex5.c
@@ -1,213 +1,202 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <math.h>
-#include <float.h>
 
-double factorial(int n)
+enum ret_type_t
 {
-    if (n < 0)
+    SUCCESS,    // Успех
+    ERROR_NAN,  // Не является числом
+    ERROR_FULL, // Переполнение
+    ERROR_ARGS, // Количество аргументов неверное
+    ERROR_EPS   // Слишком большое значение эпсилон
+};
+typedef int (*callback)(double, double);
+
+// Перевод из строкового представления в double-овое
+double chToD(char *x)
+{
+    double xD = 0;
+    double m = 10;
+    double mBuf = m;
+    int flag = 1;
+
+    for (int i = 0; i < (int)strlen(x); ++i)
     {
-        return -1;
-    }
-    if (n == 0)
-    {
-        return 1;
-    }
-    double result = 1.0;
-    for (int i = 1; i <= n; ++i)
-    {
-        if (DBL_MAX / result < i)
+        if (x[i] == '.')
         {
-            return -2;
+            flag = 0;
+            m = 10.0;
+            mBuf = m;
+            continue;
         }
-        result *= i;
+        if (flag)
+        {
+            xD = xD * m + (x[i] - '0');
+        }
+        else
+        {
+            xD = xD + ((double)(x[i] - '0')) / m;
+            m *= 10.0;
+            if (mBuf > m)
+            {
+                return ERROR_FULL;
+            }
+            mBuf = m;
+        }
     }
-    return result;
+    return xD;
 }
 
-double double_factorial(int n)
+// Вычисляет сумму под буквой a
+int funcForA(double eps, double x)
 {
-    if (n < -1)
+    double prevEl = 1.0;
+    double prevSum = prevEl;
+
+    for (int i = 1; i < 1000000; ++i)
     {
-        return -1;
-    }
-    if (n <= 1)
-    {
-        return 1;
-    }
-    double res = 1.0;
-    for (int i = n; i >= 2; i -= 2)
-    {
-        if (DBL_MAX / res < i)
+        double nowEl = prevEl * x / (double)i;
+        double nowSum = prevSum + nowEl;
+        if ((fabs(nowSum - prevSum) < eps) || (fabs(nowEl) < eps))
         {
-            return -2;
+            printf("The result of sum A is: %f\n", nowSum);
+            return SUCCESS;
         }
-        res *= i;
+        prevEl = nowEl;
+        prevSum = nowSum;
     }
-    return res;
+    return ERROR_EPS;
 }
 
-int calculate_sum_a(double x, double epsilon, double *result)
+// Вычисляет сумму под буквой b
+int funcForB(double eps, double x)
 {
-    if (x == 0.0)
-    {
-        *result = 1.0;
-        return 0;
-    }
+    double prevEl = 1.0;
+    double prevSum = prevEl;
 
-    double sum = 1.0;
-    double term = 1.0;
-    int n = 1;
-
-    do
+    for (int i = 1; i < 1000000; ++i)
     {
-        term *= x / n;
-        if (isinf(term))
+        double nowEl = prevEl * (-1.0) * x * x / (2 * (i - 1) + 1) / (2 * i);
+        double nowSum = prevSum + nowEl;
+        if ((fabs(nowSum - prevSum) < eps) || (fabs(nowEl) < eps))
         {
-            return 1;
+            printf("The result of sum B is: %f\n", nowSum);
+            return SUCCESS;
         }
-        sum += term;
-        n++;
-    } while (fabs(term) > epsilon);
-
-    *result = sum;
-    return 0;
+        prevEl = nowEl;
+        prevSum = nowSum;
+    }
+    return ERROR_EPS;
 }
 
-int calculate_sum_b(double x, double epsilon, double *result)
+// Вычисляет сумму под буквой c
+int funcForC(double eps, double x)
 {
-    if (x == 0)
-    {
-        *result = 1.0;
-        return 0;
-    }
+    double prevEl = 1.0;
+    double prevSum = prevEl;
 
-    double sum = 1.0;
-    double term = 1.0;
-    int n = 1;
-
-    do
+    for (int i = 1; i < 1000000; ++i)
     {
-        if (isinf(term))
+        double nowEl = prevEl * 27 * pow(i, 3) * x * x / (3 * (i - 1) + 1) / (3 * (i - 1) + 2) / (3 * i);
+        double nowSum = prevSum + nowEl;
+        if ((fabs(nowSum - prevSum) < eps) || (fabs(nowEl) < eps))
         {
-            return 1;
+            printf("The result of sum C is: %f\n", nowSum);
+            return SUCCESS;
         }
-        term *= -x * x / ((2.0 * n - 1) * (2.0 * n));
-        sum += term;
-        n++;
-    } while (fabs(term) > epsilon);
-
-    *result = sum;
-    return 0;
+        prevEl = nowEl;
+        prevSum = nowSum;
+    }
+    return ERROR_EPS;
 }
 
-int calculate_sum_c(double x, double epsilon, double *result)
+// Вычисляет сумму под буквой d
+int funcForD(double eps, double x)
 {
-    if (x == 0)
-    {
-        *result = 1.0;
-        return 0;
-    }
+    double prevEl = -0.5 * x * x;
+    double prevSum = prevEl;
 
-    double sum = 1.0;
-    double term = 1.0;
-    int n = 1;
-
-    do
+    for (int i = 1; i < 1000000; ++i)
     {
-        if (isinf(term))
+        double nowEl = prevEl * (-1.0) * (x * x) * (2.0 * (double)(i - 1) + 1) / 2.0 / (double)i;
+        double nowSum = prevSum + nowEl;
+        if ((fabs(nowSum - prevSum) < eps) || (fabs(nowEl) < eps))
         {
-            return 1;
+            printf("The result of sum D is: %f\n", nowSum);
+            return SUCCESS;
         }
-        term *= 27.0 * x * x / ((3.0 * n - 2) * (3.0 * n - 1) * (3.0 * n));
-        sum += term;
-        n++;
-    } while (fabs(term) > epsilon);
-
-    *result = sum;
-    return 0;
+        prevEl = nowEl;
+        prevSum = nowSum;
+    }
+    return ERROR_EPS;
 }
 
-int calculate_sum_d(double x, double epsilon, double *result)
+// Проверяет является ли строковое представление вещественным числом
+int checkNumber(char *x)
 {
-    if (x == 0)
+    int flag = 0;
+    for (int i = 0; i < (int)strlen(x); i++)
     {
-        *result = 0;
-        return 0;
-    }
-
-    double sum = 0.0;
-    double term = -x * x / 2.0;
-    int n = 1;
-
-    do
-    {
-        if (isinf(term))
+        if ((x[i] < '0' || x[i] > '9') && (x[i] != '.'))
         {
-            return 1;
+            return ERROR_NAN;
         }
-        sum += term;
-        n++;
-        term *= -x * x * (2.0 * n - 1.0) / (2.0 * n * (2.0 * n - 2.0));
-    } while (fabs(term) > epsilon);
-
-    *result = sum;
-    return 0;
+        if (x[i] == '.')
+        {
+            flag++;
+            if (flag > 1)
+            {
+                return ERROR_NAN;
+            }
+        }
+    }
+    return SUCCESS;
 }
 
 int main(int argc, char *argv[])
 {
+
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <x_value> <epsilon_value>\n", argv[0]);
-        return 1;
+        printf("Wrong number of arguments");
+        return ERROR_ARGS;
     }
 
-    char *endptr;
-    double x = strtod(argv[1], &endptr);
-    if (*endptr != '\0' || x == HUGE_VAL || x == -HUGE_VAL)
+    if (checkNumber(argv[1]) || checkNumber(argv[2]))
     {
-        fprintf(stderr, "Invalid x value.\n");
-        return 1;
+        printf("There must be an epsilon and x");
+        return ERROR_NAN;
     }
 
-    double epsilon = strtod(argv[2], &endptr);
-    if (*endptr != '\0' || epsilon == HUGE_VAL || epsilon == -HUGE_VAL || epsilon <= 0)
+    if (chToD(argv[1]) == ERROR_FULL && ((int)strlen(argv[1]) > 3))
     {
-        fprintf(stderr, "Invalid epsilon value.\n");
-        return 1;
+        printf("Your number is too big");
+        return ERROR_FULL;
+    }
+    if (chToD(argv[2]) == ERROR_FULL && ((int)strlen(argv[2]) > 3))
+    {
+        printf("Your number is too big");
+        return ERROR_FULL;
     }
 
-    double result_a, result_b, result_c, result_d;
+    double eps = chToD(argv[1]);
+    double x = chToD(argv[2]);
+    callback cbs[] = {&funcForA, &funcForB, &funcForC, &funcForD};
+    for (int i = 0; i < 4; ++i)
+    {
+        callback find = cbs[i];
 
-    if (calculate_sum_a(x, epsilon, &result_a) != 0)
-    {
-        fprintf(stderr, "Error calculating sum a.\n");
-        return 1;
+        if (find(eps, x))
+        {
+            printf("Your epsilon is too big for current sum");
+            return ERROR_EPS;
+        }
     }
-    printf("a: %lf\n", result_a);
-    if (calculate_sum_b(x, epsilon, &result_b) != 0)
-    {
-        fprintf(stderr, "Error calculating sum b.\n");
-        return 1;
-    }
-    printf("b: %lf\n", result_b);
-    if (calculate_sum_c(x, epsilon, &result_c) != 0)
-    {
-        fprintf(stderr, "Error calculating sum c.\n");
-        return 1;
-    }
-    printf("c: %lf\n", result_c);
-    if (calculate_sum_d(x, epsilon, &result_d) != 0)
-    {
-        fprintf(stderr, "Error calculating sum d.\n");
-        return 1;
-    }
-    printf("d: %lf\n", result_d);
 
     return 0;
 }
 
 // gcc ex5.c -o ex5
 
-// ./ex5.exe 1.5 0.00001
+// ./ex5.exe 0.00001 1.5
